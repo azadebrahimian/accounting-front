3345e021ee68cdc50a631d5df2e7450fcc01334b
ebrahimiana <ebrahimian.a@husky.neu.edu>
2022-02-11 14:08:12 -0500

    no more mui

--- src/CreateTxn.tsx
+++ src/CreateTxn.tsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-// import { TextField, RadioGroup, FormControlLabel, Radio } from "@mui/material";
-// import { LocalizationProvider, DatePicker } from "@mui/lab";
-// import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import { isUserInputPriceInvalid } from "./util/UserInputUtil";
 
 import "./CreateTxn.scss";
@@ -11,58 +8,73 @@ function CreateTxn() {
     const [amount, setAmount] = useState(0);
     const [transactionType, setTransactionType] = useState("spent");
     const [location, setLocation] = useState("");
-    const [date, setDate] = useState(null);
+    const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
 
     const amountErrorMessage =
         "Amount entered must be a non-negative, valid price.";
-    console.log(isUserInputPriceInvalid("455"));
-
     return (
         <div className="create-main">
             <h1>Create a new transaction here.</h1>
             <form className="create-form">
-                <input type="checkbox" />
-                {/* <TextField
-                    variant="standard"
-                    label="Amount"
-                    error={amountError}
-                    helperText={amountError && amountErrorMessage}
-                    onChange={(e) => {
-                        if (isUserInputPriceInvalid(e.target.value)) {
-                            setAmountError(true);
-                        } else {
-                            setAmount(Number(e.target.value));
-                            setAmountError(false);
-                        }
-                    }}
-                />
-                <TextField
-                    variant="standard"
-                    label="Location"
-                    onChange={(e) => setLocation(e.target.value)}
-                /> */}
-                {/* <RadioGroup row value={transactionType}>
-                    <FormControlLabel
-                        value="spent"
-                        label="Spent"
-                        control={<Radio />}
+                <div className="create-form-individual-item">
+                    <label>Amount:</label>
+                    <input
+                        type="text"
+                        onChange={(e) => {
+                            if (isUserInputPriceInvalid(e.target.value)) {
+                                setAmountError(true);
+                            } else {
+                                setAmount(Number(e.target.value));
+                                setAmountError(false);
+                            }
+                        }}
                     />
-                    <FormControlLabel
-                        value="received"
-                        label="Received"
-                        control={<Radio />}
+                </div>
+                {amountError && (
+                    <div className="amount-error-message">
+                        <p>{amountErrorMessage}</p>
+                    </div>
+                )}
+                <div className="create-form-individual-item">
+                    <label>Location:</label>
+                    <input
+                        type="text"
+                        onChange={(e) => {
+                            setLocation(e.target.value);
+                        }}
                     />
-                </RadioGroup> */}
-                {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <DatePicker
-                        label="Date"
+                </div>
+                <div className="create-form-individual-item">
+                    <label>Spent</label>
+                    <input
+                        type="radio"
+                        checked={transactionType === "spent"}
+                        onChange={() => {
+                            setTransactionType("spent");
+                        }}
+                    />
+                    <label>Received</label>
+                    <input
+                        type="radio"
+                        checked={transactionType === "received"}
+                        onChange={() => {
+                            setTransactionType("received");
+                        }}
+                    />
+                </div>
+                <div className="create-form-individual-item">
+                    <label>Date:</label>
+                    <input
+                        type="date"
                         value={date}
                         onChange={(e) => {
-                            setDate(e);
+                            setDate(e.target.value);
                         }}
-                        renderInput={(params) => <TextField {...params} />}
                     />
-                </LocalizationProvider> */}
+                </div>
+                <button type="submit" className="form-submit-button">
+                    Submit
+                </button>
             </form>
         </div>
     );
