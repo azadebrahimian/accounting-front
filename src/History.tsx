301b366c1652c24926c20f2d87db92d781174d6f
ebrahimiana <ebrahimian.a@husky.neu.edu>
2022-02-16 01:17:54 -0500

    working w api

--- src/History.tsx
+++ src/History.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import axios from "axios";
+
 function History() {
+    const [sample, setSample] = useState("");
+
+    axios.get("/history").then((res) => {
+        setSample(res.data.title);
+    });
+
     return (
-      <div className="history-main">
-        <h1>This is where you can view your transaction history.</h1>
-      </div>
+        <div className="history-main">
+            <h1>This is where you can view your transaction history.</h1>
+            <p>{sample}</p>
+        </div>
     );
-  }
-  
-  export default History;
-  
+}
+
+export default History;
