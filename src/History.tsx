e7a178462b9c5cd9f63611cb71efab481f506794
aebrahimian <ebrahimian.a@husky.neu.edu>
2022-02-23 23:22:50 -0500

    remove axios stuff

--- src/History.tsx
+++ src/History.tsx
@@ -1,19 +1,10 @@
 import { useState } from "react";
-import axios from "axios";
 
 function History() {
-    const [sample, setSample] = useState("");
-
-    axios
-        .get("https://nameless-escarpment-96617.herokuapp.com/history")
-        .then((res) => {
-            setSample(res.data.title);
-        });
 
     return (
         <div className="history-main">
             <h1>This is where you can view your transaction history.</h1>
-            <p>{sample}</p>
         </div>
     );
 }
