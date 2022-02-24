4a1925d416e4f0fe4ffea4a7862131e4c8ba5ce5
aebrahimian <ebrahimian.a@husky.neu.edu>
2022-02-24 00:43:51 -0500

    console.log

--- src/History.tsx
+++ src/History.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 function History() {
 
+    console.log('will this show');
+
     return (
         <div className="history-main">
             <h1>This is where you can view your transaction history.</h1>
