c99caab08bf6d2602e4a9ebf79d0d9633a80b052
aebrahimian <ebrahimian.a@husky.neu.edu>
2022-02-25 15:47:42 -0500

    try post txn

--- src/CreateTxn.tsx
+++ src/CreateTxn.tsx
@@ -2,82 +2,98 @@ import { useState } from "react";
 import { isUserInputPriceInvalid } from "./util/UserInputUtil";
 
 import "./CreateTxn.scss";
+import axios from "axios";
 
 function CreateTxn() {
-    const [amountError, setAmountError] = useState(false);
-    const [amount, setAmount] = useState(0);
-    const [transactionType, setTransactionType] = useState("spent");
-    const [location, setLocation] = useState("");
-    const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
+  const [amountError, setAmountError] = useState(false);
+  const [amount, setAmount] = useState(0);
+  const [transactionType, setTransactionType] = useState("spent");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
 
-    const amountErrorMessage =
-        "Amount entered must be a non-negative, valid price.";
-    return (
-        <div className="create-main">
-            <h1>Create a new transaction here.</h1>
-            <form className="create-form">
-                <div className="create-form-individual-item">
-                    <label>Amount:</label>
-                    <input
-                        type="text"
-                        onChange={(e) => {
-                            if (isUserInputPriceInvalid(e.target.value)) {
-                                setAmountError(true);
-                            } else {
-                                setAmount(Number(e.target.value));
-                                setAmountError(false);
-                            }
-                        }}
-                    />
-                </div>
-                {amountError && (
-                    <div className="amount-error-message">
-                        <p>{amountErrorMessage}</p>
-                    </div>
-                )}
-                <div className="create-form-individual-item">
-                    <label>Location:</label>
-                    <input
-                        type="text"
-                        onChange={(e) => {
-                            setLocation(e.target.value);
-                        }}
-                    />
-                </div>
-                <div className="create-form-individual-item">
-                    <label>Spent</label>
-                    <input
-                        type="radio"
-                        checked={transactionType === "spent"}
-                        onChange={() => {
-                            setTransactionType("spent");
-                        }}
-                    />
-                    <label>Received</label>
-                    <input
-                        type="radio"
-                        checked={transactionType === "received"}
-                        onChange={() => {
-                            setTransactionType("received");
-                        }}
-                    />
-                </div>
-                <div className="create-form-individual-item">
-                    <label>Date:</label>
-                    <input
-                        type="date"
-                        value={date}
-                        onChange={(e) => {
-                            setDate(e.target.value);
-                        }}
-                    />
-                </div>
-                <button type="submit" className="form-submit-button">
-                    Submit
-                </button>
-            </form>
+  const amountErrorMessage =
+    "Amount entered must be a non-negative, valid price.";
+  return (
+    <div className="create-main">
+      <h1>Create a new transaction here.</h1>
+      <form className="create-form">
+        <div className="create-form-individual-item">
+          <label>Amount:</label>
+          <input
+            type="text"
+            onChange={(e) => {
+              if (isUserInputPriceInvalid(e.target.value)) {
+                setAmountError(true);
+              } else {
+                setAmount(Number(e.target.value));
+                setAmountError(false);
+              }
+            }}
+          />
         </div>
-    );
+        {amountError && (
+          <div className="amount-error-message">
+            <p>{amountErrorMessage}</p>
+          </div>
+        )}
+        <div className="create-form-individual-item">
+          <label>Location:</label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setLocation(e.target.value);
+            }}
+          />
+        </div>
+        <div className="create-form-individual-item">
+          <label>Spent</label>
+          <input
+            type="radio"
+            checked={transactionType === "spent"}
+            onChange={() => {
+              setTransactionType("spent");
+            }}
+          />
+          <label>Received</label>
+          <input
+            type="radio"
+            checked={transactionType === "received"}
+            onChange={() => {
+              setTransactionType("received");
+            }}
+          />
+        </div>
+        <div className="create-form-individual-item">
+          <label>Date:</label>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+        </div>
+        <button
+          type="submit"
+          className="form-submit-button"
+          onClick={() => {
+            axios
+              .post("/api/transactions/", {
+                amount: amount,
+                location: location,
+                transactionType: transactionType,
+                transactionDate: date,
+              })
+              .then((res) => {
+                console.log("posted!");
+              });
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateTxn;
