1322257ee984a0ab97cab03b533008e366b5f031
ebrahimiana <ebrahimian.a@husky.neu.edu>
2022-02-28 17:45:18 -0500

    add bearer

--- src/util/auth.js
+++ src/util/auth.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default function checkIfTokenIsValid() {
     const userToken = localStorage.getItem("jwtToken") || "";
+    const tokenString = "Bearer " + userToken;
 
-    return axios.get("/api/users/auth", { headers: { "x-access-token": userToken } })
+    return axios.get("/api/users/auth", { headers: { "x-access-token": tokenString } })
 }
