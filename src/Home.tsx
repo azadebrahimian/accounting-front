deb2b415d4d7a4812f34f4ecf92792d894f1bd32
aebrahimian <ebrahimian.a@husky.neu.edu>
2022-02-28 23:35:31 -0500

    show proper values

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,60 +1,63 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { UserContext } from "./UserContext";
 
 import "./Home.scss";
 
 function Home() {
-    const { userInfo } = useContext(UserContext);
-    let weeklyTransactions = [];
-    let dailyTotal = 0;
-    let weeklyTotal = 0;
+  const { userInfo } = useContext(UserContext);
+  const [dailySpending, setDailySpending] = useState(0);
+  const [weeklySpending, setWeeklySpending] = useState(0);
+  let weeklyTransactions = [];
 
+  useEffect(() => {
+    if (userInfo) {
+      const username = userInfo.username;
+      axios.get(`/api/transactions/${username}/currentWeek`).then((res) => {
+        weeklyTransactions = res.data;
+        let dailyTotal = 0;
+        let weeklyTotal = 0;
 
-    useEffect(() => {
-        if (userInfo) {
-            const username = userInfo.username;
-            axios
-                .get(`/api/transactions/${username}/currentWeek`)
-                .then((res) => {
-                    weeklyTransactions = res.data;
+        const today = new Date();
+        weeklyTransactions.forEach((wt) => {
+          if (
+            new Date(
+              wt.transactionDate.replace(/-/g, "/").replace(/T.+/, "")
+            ).getDate() === today.getDate()
+          ) {
+            dailyTotal += wt.amount;
+          }
 
-                    const today = new Date();
-                    weeklyTransactions.forEach((wt) => {
-                        if (new Date(wt.transactionDate.replace(/-/g, '\/').replace(/T.+/, '')).getDate() === today.getDate()) {
-                            dailyTotal += wt.amount;
-                        }
+          weeklyTotal += wt.amount;
+        });
 
-                        weeklyTotal += wt.amount;
-                    });
-                });
-        }
-    });
-
-    if (!userInfo) {
-        return (
-            <div className="home-main">
-                <h1>Please login</h1>
-            </div>
-        );
+        setDailySpending(dailyTotal);
+        setWeeklySpending(weeklyTotal);
+      });
     }
+  });
 
-    console.log("outside effect");
-    console.log(weeklyTotal);
-
+  if (!userInfo) {
     return (
-        <div className="home-main">
-            <h1>Welcome back {userInfo.username}.</h1>
-            <h3>Today you spent: ${dailyTotal}</h3>
-            <h3>So far this week you spent: ${weeklyTotal}</h3>
-            <div className="remaining-week-section">
-                <h2>
-                    Remaining balance this week:{" "}
-                    <span className="remaining-balance">$99</span>
-                </h2>
-            </div>
-        </div>
+      <div className="home-main">
+        <h1>Please login</h1>
+      </div>
     );
+  }
+
+  return (
+    <div className="home-main">
+      <h1>Welcome back {userInfo.username}.</h1>
+      <h3>Today you spent: ${dailySpending}</h3>
+      <h3>So far this week you spent: ${weeklySpending}</h3>
+      <div className="remaining-week-section">
+        <h2>
+          Remaining balance this week:{" "}
+          <span className="remaining-balance">$99</span>
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
